e5ecb8460bc4d2889aae30a56900ca3e5cdcb3c8
CarloRAccenture <carlo.jose.n.ruste@accenture.com>
2025-11-27 05:37:35 +0000

    Commit Rest APIs

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-z1^2wosw5m*c=vz9jahnoind3+2m1e%4dn=!lq-5g!ft*!z_=4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if os.environ.get('CODESPACE_NAME'):
+    ALLOWED_HOSTS.append(f"{os.environ.get('CODESPACE_NAME')}-8000.app.github.dev")
 
 
 # Application definition
